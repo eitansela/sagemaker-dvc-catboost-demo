f070169683058ef7d7428f73acc350e7b1a7263b
Eitan Sela <eitan.sela@gmail.com>
2021-10-12 18:31:37 +0300

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# sagemaker-dvc-catboost-demo
+## Amazon SageMaker Local Mode with DVC Example
+
+This repository contains example and related resources showing you how to train a model with California housing dataset, fetched using DVC, and serve on your local machine using Amazon SageMaker Local mode. 
+
+## Overview
+
+### SageMaker Local Mode
+The local mode in the Amazon SageMaker Python SDK can emulate CPU (single and multi-instance) and GPU (single instance) SageMaker training jobs by changing a single argument in the TensorFlow, PyTorch or MXNet estimators.  To do this, it uses Docker compose and NVIDIA Docker.  It will also pull the Amazon SageMaker TensorFlow, PyTorch or MXNet containers from Amazon ECS, so you’ll need to be able to access a public Amazon ECR repository from your local environment.
+
+[Read the blog post](https://aws.amazon.com/blogs/machine-learning/use-the-amazon-sagemaker-local-mode-to-train-on-your-notebook-instance/)
+
+### California Housing dataset
+We use the California Housing dataset, present in Scikit-Learn: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_california_housing.html. The California Housing dataset was originally published in:
+
+Pace, R. Kelley, and Ronald Barry. "Sparse spatial auto-regressions." Statistics & Probability Letters 33.3 (1997): 291-297.
+
+### Data Version Control · DVC
+DVC is built to make ML models shareable and reproducible. It is designed to handle large files, data sets, machine learning models, and metrics as well as code.
+
+[DVC Official Site](https://dvc.org/)
--- catboost_bring_your_own_container_local_training_with_dvc.py
+++ catboost_bring_your_own_container_local_training_with_dvc.py
@@ -14,7 +14,7 @@ import pandas as pd
 from sagemaker.estimator import Estimator
 from sagemaker.local import LocalSession
 from sagemaker.predictor import csv_serializer
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 
 sagemaker_session = LocalSession()
@@ -22,29 +22,47 @@ sagemaker_session.config = {'local': {'local_code': True}}
 
 # For local training a dummy role will be sufficient
 role = 'arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20200101T000001'
-
-
 image = 'sagemaker-catboost-dvc-local'
 
-local_regressor = Estimator(
-    image,
-    role,
-    instance_count=1,
-    instance_type="local",
-    hyperparameters={
-        "dvc-repo-url": "https://github.com/eitansela/sagemaker-dvc-catboost-demo",
-        "dvc-branch-name":  "dev_dataset_1"
-    },
-)
 
-local_regressor.fit()
+def main():
+    # Prepare data for testing model predictions
+    data = fetch_california_housing()
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.data, data.target, test_size=0.25, random_state=42
+    )
+    # we don't train a model, so we will need only the testing data
+    testX = pd.DataFrame(X_test, columns=data.feature_names)
 
-# predictor = local_regressor.deploy(1, 'local', serializer=csv_serializer)
-#
-# with open(local_test, 'r') as f:
-#     payload = f.read().strip()
-#
-# predicted = predictor.predict(payload).decode('utf-8')
-# print(predicted)
-#
-# predictor.delete_endpoint()
+    # We use the Estimator from the SageMaker Python SDK
+    local_regressor = Estimator(
+        image,
+        role,
+        instance_count=1,
+        instance_type="local",
+        hyperparameters={
+            "dvc-repo-url": "https://github.com/eitansela/sagemaker-dvc-catboost-demo",
+            "dvc-branch-name":  "dev_dataset_1"
+        },
+    )
+
+    # Start model training
+    print("Starting model training.")
+    local_regressor.fit()
+    print('Completed model training.')
+
+    # Deploy endpoint in local mode
+    print('Deploying endpoint in local mode.')
+    predictor = local_regressor.deploy(1, 'local', serializer=csv_serializer)
+
+    # Invoke local mode endpoint to get predictions
+    print('Invoking local mode endpoint to get predictions.')
+    predicted = predictor.predict(testX[data.feature_names].to_csv(header=False, index=False)).decode('utf-8')
+    print(predicted)
+
+    # Delete the local endpoint
+    print('About to delete the endpoint to stop paying (if in cloud mode).')
+    predictor.delete_endpoint()
+
+if __name__ == "__main__":
+    main()
